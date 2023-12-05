79ec8d41d147cc7cfb38a30e7ccaa321e684346a
Pratham Shah <91117190+PrathamShah10@users.noreply.github.com>
2023-12-05 16:36:31 +0530

    Added comments for easy use

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,3 +1,4 @@
+// this is 0-indexed seg tree
 class segtree {
     vector<int>seg;
 public:
@@ -5,6 +6,7 @@ public:
         seg.resize(4 * n + 1);
     }
     void build(int ind, int low, int high, vector<int>& arr) {
+        // use: build(0, 0, n - 1, arr); for building
         if (low == high) {
             seg[ind] = arr[low];
             return;
@@ -15,20 +17,20 @@ public:
         seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2];
     }
     int query(int ind, int low, int high, int l, int r) {
-        //[l r] is given range
-
-        //no overlap
-        //[low high] [l r] or  [l r] [low high]
+        // [l r] is given range
+        // ind = 0, low = 0, high = n - 1
+        // no overlap
+        // [low high] [l r] or  [l r] [low high]
         if (l > high || low > r) {
             return 0;
         }
-        //complete overlap
-        //here the seg range is completely inside [l r]
-        //[l low high r]
+        // complete overlap
+        // here the seg range is completely inside [l r]
+        // [l low high r]
         else if (low >= l && high <= r) {
             return seg[ind];
         }
-        //partial overlap
+        // partial overlap
         else {
             int mid = (low + high) >> 1;
             int left = query(2 * ind + 1, low, mid, l, r);
@@ -37,7 +39,8 @@ public:
         }
     }
     void update(int ind, int low, int high, int updateIndex, int updateValue) {
-        //ind -> segment tree's index
+        // ind -> segment tree's index
+        // ind = 0, low = 0, high = n - 1
         int mid = (low + high) >> 1;
         if (low == high) {
             seg[ind] = updateValue;
